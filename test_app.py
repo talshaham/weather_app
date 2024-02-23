f0e1cca10b896989893897c80436cbd1f5b21e7a
tal.shaham <talso12345@gmail.com>
2024-02-23 15:11:12 +0200

    including test

---
+++ test_app.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def app_reachable_test():
+    try:
+        res = requests.get("http://127.0.0.1:5000").status_code
+    except requests.exceptions.ConnectionError:
+        print("website isn't reachable")
+    else:
+        assert res in range(200, 400)
+        print("website is reachable")
+
+
+app_reachable_test()
