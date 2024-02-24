4b6ed1a8dc11948ebe97af9c12dda30cf745fae8
tal.shaham <talso12345@gmail.com>
2024-02-24 10:59:47 +0200

    hope ok

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import requests
 
 def app_reachable_test():
     try:
-        res = requests.get("http://16.171.116.132:80").status_code
+        res = requests.get("http://51.20.60.83/").status_code
     except requests.exceptions.ConnectionError:
         print("website isn't reachable")
     else:
